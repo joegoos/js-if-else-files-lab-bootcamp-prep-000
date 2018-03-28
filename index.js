2bcd11a11bba8e3cbb2e53b30db2f758e0331b34
Joseph Guastamacchia <joeguasta@gmail.com>
2018-03-28 22:37:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var name = "joe"
-
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
 var height = "74"
 
 // Don't worry about this
